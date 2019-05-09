f959ace77fc13bd4831d49c89050f074185b3f81
jaquelinne28 <lealjaquie02@gmail.com>
2019-05-09 07:07:39 -0500

    05
    des

--- js/acciones.js
+++ js/acciones.js
@@ -1,29 +1,26 @@
 //JavaScript Document
 
-$(document).ready(function (e) {
+$(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion"."Aceptar"});
+			navigator.notification.alert("Desliza a la izquierda",function(){"Aplicacion ","Aceptar"});
 		});
 		
 		$('#derecha').on("swiperight",function(){
-			navigator.notification. confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(1);
-			break;
-			
-			case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
+			navigator.notification.confirm("¡Que quieres hacer?",function(opt){
+				switch(opt)
+				{
+					case 1:
+					navigator.notification.beep(1);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(1000);
+					break;
+				}
 			},"Aplicacion ","Beep,Vibrar,Cancelar");
 			
 		});
-	},false);
-});
-			
-					
-				
+	      },false);
+        });
